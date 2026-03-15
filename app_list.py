d4592e7ad2d7a74ecdb80b6aec134cbb4ac18adc
Felipe Ferri <felipe.ferri07@gmail.com>
2026-03-15 20:17:55 -0300

    Criação de uma app que preve mais de uma casa

---
+++ app_list.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+from typing import List
+
+from KNN_teste import KNN_felipe
+
+app = FastAPI()
+
+# carregar dataset apenas uma vez
+dataset = pd.read_csv("data/train_data.csv")
+variables =  ["bedrooms", "bathrooms", "sqft_living", "sqft_lot", "floors", "sqft_above", "lat", "long", "house_age"]
+K = 5
+
+
+class House(BaseModel):
+
+    sqft_living: float
+    bedrooms: int
+    bathrooms: float
+    sqft_lot: float
+    floors: int
+    sqft_above: int
+    lat: float
+    long: float
+    house_age: int
+
+
+@app.post("/predict")
+def predict(data: List[House]):
+
+
+    sample_df = pd.DataFrame([d.model_dump() for d in data])
+
+    prices = list()
+    for i in sample_df.index:
+        prices.append(KNN_felipe(t_data = dataset, variables = variables, sample = sample_df.iloc[i], neighborhood= K))
+
+    return {"predicted_price": prices}
